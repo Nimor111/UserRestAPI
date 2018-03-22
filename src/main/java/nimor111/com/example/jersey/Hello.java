ad9b3e9b28379c30e867b81aab74366822ca5069
Nimor111 <georgi.bojinov@hotmail.com>
2018-03-22 22:36:02 +0200

    Initial commit

---
+++ src/main/java/nimor111/com/example/jersey/Hello.java
@@ -0,0 +1,20 @@
+package nimor111.com.example.jersey;
+
+public class Hello {
+
+    String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
+}
