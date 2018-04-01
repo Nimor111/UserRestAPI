b3e58057aa5be163adbfe00b4c067833c3cd925b
Nimor111 <georgi.bojinov@hotmail.com>
2018-04-01 16:24:10 +0300

    Add some basic integration tests.

---
+++ src/test/java/nimor111/com/example/jersey/FunctionalTest.java
@@ -0,0 +1,31 @@
+package nimor111.com.example.jersey;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+public class FunctionalTest {
+
+    @BeforeClass
+    public static void setup() {
+        String port = System.getProperty("server.port");
+        if (port == null) {
+            RestAssured.port = Integer.valueOf(8081);
+        } else {
+            RestAssured.port = Integer.valueOf(port);
+        }
+
+        String basePath = System.getProperty("server.base");
+        if (basePath == null) {
+            basePath = "/api/";
+        }
+        RestAssured.basePath = basePath;
+
+        String baseHost = System.getProperty("server.host");
+        if (baseHost == null) {
+            baseHost = "http://localhost";
+        }
+        RestAssured.baseURI = baseHost;
+
+    }
+
+}
---
+++ src/test/java/nimor111/com/example/jersey/UserResourceTest.java
@@ -0,0 +1,90 @@
+package nimor111.com.example.jersey;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class UserResourceTest extends FunctionalTest {
+    Faker faker = new Faker();
+
+    @Test
+    public void getUsersReturn200() {
+        given().when().get("/users/get").then().statusCode(200);
+    }
+
+    @Test
+    public void wrongEmailFormatReturns404() {
+        given().when().get("/users/get/123").then().statusCode(404);
+    }
+
+    @Test
+    public void creatingUserWithValidDataReturns200() {
+        Map<String, String> user = new HashMap<String, String>();
+        user.put("firstName", this.faker.name().firstName());
+        user.put("lastName", this.faker.name().lastName());
+        user.put("email", this.faker.name().firstName() + "@" + this.faker.name().lastName() + ".com");
+        user.put("role", "User");
+
+        given().contentType("application/json").body(user).when().post("/users/create").then().statusCode(200);
+    }
+
+    @Test
+    public void updatingUserReturns200AndIsSuccessful() {
+        Map<String, String> user = new HashMap<String, String>();
+        user.put("firstName", this.faker.name().firstName());
+        user.put("lastName", this.faker.name().lastName());
+        user.put("email", this.faker.name().firstName() + "@" + this.faker.name().lastName() + ".com");
+        user.put("role", "User");
+
+        given().contentType("application/json").body(user).when().post("/users/create").then().statusCode(200);
+
+        Map<String, String> updatedUser = new HashMap<String, String>();
+        String newName = this.faker.name().firstName();
+        updatedUser.put("firstName", newName);
+        updatedUser.put("lastName", user.get("lastName"));
+        updatedUser.put("email", user.get("email"));
+        updatedUser.put("role", user.get("role"));
+
+        given()
+            .contentType("application/json")
+            .body(updatedUser)
+            .when()
+            .put("/users/update")
+            .then()
+            .body("firstName", equalTo(newName))
+            .statusCode(200);
+    }
+
+    @Test
+    public void deletingUserReturns200() {
+        Map<String, String> user = new HashMap<String, String>();
+        user.put("firstName", this.faker.name().firstName());
+        user.put("lastName", this.faker.name().lastName());
+        user.put("email", this.faker.name().firstName() + "@" + this.faker.name().lastName() + ".com");
+        user.put("role", "User");
+
+        given().contentType("application/json")
+            .body(user)
+            .when()
+            .post("/users/create")
+            .then()
+            .statusCode(200);
+
+        given().contentType("application/json")
+            .when()
+            .delete("/users/delete/" + user.get("email"))
+            .then()
+            .statusCode(200);
+
+        given()
+            .when()
+            .get("/users/get/" + user.get("email"))
+            .then()
+            .statusCode(404);
+    }
+}
